628beb9ecfb3028e0020e7eb5b4879cf15a9c2eb
Anindita Roy <anindita.roy.mu@gmail.com>
2022-05-26 13:39:27 +0600

    project setup initialized and navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: "#FFAA01",
+          secondary: "#e76f51",
+          accent: "#191919",
+          neutral: "#edede9",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+}
