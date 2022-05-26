3be43c14f9952b1e862c6415d015180d048b8f23
Anindita Roy <anindita.roy.mu@gmail.com>
2022-05-27 01:36:08 +0600

    summary stat style modified

--- src/Pages/Home/SummaryStat.js
+++ src/Pages/Home/SummaryStat.js
@@ -28,7 +28,7 @@ const SummaryStat = () => {
                             <div class="flex flex-row justify-between group-hover:text-gray-200">
                                 <p>Sell Rate</p>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 group-hover:text-gray-200"
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary group-hover:text-gray-200"
                                         viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd"
                                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -52,7 +52,7 @@ const SummaryStat = () => {
                             <div class="flex flex-row justify-between group-hover:text-gray-200">
                                 <p>Shipped Cuntries</p>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 group-hover:text-gray-200"
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary group-hover:text-gray-200"
                                         viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd"
                                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -76,7 +76,7 @@ const SummaryStat = () => {
                             <div class="flex flex-row justify-between group-hover:text-gray-200">
                                 <p>Feedback</p>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 group-hover:text-gray-200"
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary group-hover:text-gray-200"
                                         viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd"
                                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
@@ -101,7 +101,7 @@ const SummaryStat = () => {
                             <div class="flex flex-row justify-between group-hover:text-gray-200">
                                 <p>Happy Clients</p>
                                 <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-indigo-600 group-hover:text-gray-200"
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-primary group-hover:text-gray-200"
                                         viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd"
                                             d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
